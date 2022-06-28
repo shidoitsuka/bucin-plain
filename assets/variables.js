4ffe68a2d919994766763e55f824406e3a5e5d2b
dngrozz8 <108339400+dngrozz8@users.noreply.github.com>
2022-06-28 18:28:48 +0700

    Update variables.js

--- assets/variables.js
+++ assets/variables.js
@@ -7,13 +7,13 @@ const variables = {
     night: "Good Night",
   },
   quotes: [
-    { quote: "I'm sorry for loving you.", author: "standinhsd" },
-    { quote: "I love you.", author: "Brian" },
-    { quote: "Aku sayang kamu!", author: "Ian" },
-    { quote: "Ian sayang Ava ♥", author: "Ian-nya Ava!" },
-    { quote: "Love chuuu", author: "Bribri" },
+    { quote: "I'm sorry for loving you.", author: "diva" },
+    { quote: "I love you.", author: "Diva" },
+    { quote: "Aku sayang kamu!", author: "Diva" },
+    { quote: "Diva sayang Kezia ♥", author: "pacarnya kezia!" },
+    { quote: "Love uuu sayangkuu", author: "diva" },
   ],
-  nicknames: ["Ava", "Avakuu", "Sayang", "Babe", "Cintaku", "Cinta"],
+  nicknames: ["kezia", "cantiku", "Sayang", "Babe", "Cintaku", "Cinta"],
   generated: ["{greetings}, {nicknames}!"],
 };
 
